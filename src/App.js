31ee29abbeba8fc03e3a0b699bd953aecfcb7dbf
Julian <salamanca.julian@correounivalle.edu.co>
2022-05-25 11:26:26 -0500

    Se adiciona lo de salgado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import MainWindow from './MainWindow'
 import React from 'react'
+import Inicio from './inicio/Inicio'
+import Dashboard from './dashboard/Dashboard'
+import ProtectedRoute from './auth0/ProtectedRoute'
+import ExternalApi from './external-api'
+import Blog from './blog/Blog'
+import {
+
+  Routes,
+  Route
+} from 'react-router-dom'
 
 export default function App () {
   return (
-    <MainWindow />
+    <Routes>
+    <Route path="/" element={<Blog/>}/>
+
+    <Route path="/Dashboard"element={<ProtectedRoute component={MainWindow} />}>
+      <Route
+        index
+        element={
+          <Inicio />
+        }
+      />
+      <Route path="data" element={<ExternalApi />} />
+      <Route path="inicio" element={<Inicio />} />
+    </Route>
+
+  </Routes>
   )
 }
--- src/dashboard/listItems.js
+++ src/dashboard/listItems.js
@@ -5,8 +5,6 @@ import ListItemText from '@mui/material/ListItemText'
 import ListSubheader from '@mui/material/ListSubheader'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import AdjustIcon from '@mui/icons-material/Adjust'
-import LoginIcon from '@mui/icons-material/Login'
-import AddReactionIcon from '@mui/icons-material/AddReaction'
 import BarChartIcon from '@mui/icons-material/BarChart'
 import LayersIcon from '@mui/icons-material/Layers'
 import AssignmentIcon from '@mui/icons-material/Assignment'
@@ -14,30 +12,19 @@ import { Link } from 'react-router-dom'
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton component={Link} to={'/inicio'}>
+    <ListItemButton component={Link} to={'inicio'}>
       <ListItemIcon>
         <AdjustIcon />
       </ListItemIcon>
       <ListItemText primary="Inicio" />
     </ListItemButton>
-    <ListItemButton component={Link} to={'/dashboard'}>
+    <ListItemButton component={Link} to={'data'}>
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItemButton>
-    <ListItemButton component={Link} to={'/sign-in'}>
-      <ListItemIcon>
-        <LoginIcon />
-      </ListItemIcon>
-      <ListItemText primary="Login" />
-    </ListItemButton>
-    <ListItemButton component={Link} to={'/sign-up'}>
-      <ListItemIcon>
-        <AddReactionIcon />
-      </ListItemIcon>
-      <ListItemText primary="Sign up" />
-    </ListItemButton>
+
     <ListItemButton>
       <ListItemIcon>
         <BarChartIcon />
